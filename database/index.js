7beab712881eeaaf6c8f2595d23b162b212a3064
Neil Martin <nnmartin94@gmail.com>
2018-10-16 16:52:47 -0700

    exercise 1 solution code

--- database/index.js
+++ database/index.js
@@ -1,1 +1,8 @@
-// TODO
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/todolist");
+
+const connection = mongoose.connection;
+connection.on("error", console.error.bind(console, "connection error"));
+connection.once("open", () => console.log("connected to mongodb"));
+
+module.exports = connection;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-// TODO
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const connection = require("../database/");
+
+const server = express();
+
+const port = 3000;
+
+server.use(morgan("dev"));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.listen(port, () => console.log(`connected to port ${port}`));
